b7be5b2a8cb319234e2f4213abe4a8311d8fbb5b
Xiaohu Cao <xiaohu.cao@nxp.com>
2026-02-17 20:19:19 +0800

    serial

--- b.c
+++ b.c
@@ -7,25 +7,27 @@
 #include "board.h"
 #include "app.h"
 
+void config(void);
 void read_line(void);
 
 #define buffer_size (100)
 static char buffer[buffer_size + 1];
 
+static const GPIO_Type *gpios[] = GPIO_BASE_PTRS;
+static const PORT_Type *ports[] = PORT_BASE_PTRS;
+
 #ifdef PORT_CLOCKS
-clock_ip_name_t port_clocks[] = PORT_CLOCKS;
+static const clock_ip_name_t port_clocks[] = PORT_CLOCKS;
 #endif
-#ifdef GPIO_CLOCKS
-clock_ip_name_t gpio_clocks[] = GPIO_CLOCKS;
-#endif
-#ifdef GPIO_BASE_PTRS
-GPIO_Type *gpios[] = GPIO_BASE_PTRS;
-#endif
-#ifdef PORT_BASE_PTRS
-PORT_Type *ports[] = PORT_BASE_PTRS;
+#ifdef PORT_RSTS
+static const SYSCON_RSTn_t resets[] = PORT_RSTS;
 #endif
 
-static const port_pin_config_t port_config_as_gpio = {
+static const gpio_pin_config_t gpio_pin_config_output_0 = {.pinDirection = kGPIO_DigitalOutput, .outputLogic = 0U};
+static const gpio_pin_config_t gpio_pin_config_output_1 = {.pinDirection = kGPIO_DigitalOutput, .outputLogic = 1U};
+static const gpio_pin_config_t gpio_pin_config_input    = {.pinDirection = kGPIO_DigitalInput};
+
+static const port_pin_config_t port_pin_config_gpio = {
     .pullSelect          = kPORT_PullDisable,
     .pullValueSelect     = kPORT_LowPullResistor,
     .slewRate            = kPORT_FastSlewRate,
@@ -41,11 +43,57 @@ static const port_pin_config_t port_config_as_gpio = {
     .lockRegister = kPORT_UnlockRegister //
 };
 
-int write_gpio(char *content);
-int read_gpio(char *content);
-int reset(char *content);
+static uint8_t pins[4][33] = {
+#if 1 // frdmmcxc353
+      // gpio 0
+    {
+        //
+        6,                           // i2c1 scl
+        7,                           // i2c1 sda
+        0xff,                        //
+        0, 1, 2, 3, 4, 5, 6, 7, 0xff //
+    },
+    // gpio 1
+    {
+        //
+        0,                                 // spi0 sck
+        1,                                 // spi0 sdi
+        2,                                 // spi0 sdo / pwt0 in0
+        3,                                 // spi0 pcs2 / pwm0 a0
+        0xff,                              //
+        0, 1, 2, 3, 8, 9, 10, 11, 12, 0xff //
+    },
+    // gpio 2
+    {
+#if 0                                                                // debug console uart0
+            0, 1,
+#endif
+        0xff, 2, 3, 4, 5, 6, 7, 16, 17, 18, 19, 20, 21, 22, 23, 0xff //
+    },
+    // gpio 3
+    {0xff, 0, 1, 2, 3, 12, 13, 14, 15, 27, 28, 0xff},
+#endif
+#if 0 // kw47
+      // gpio a
+    {0xff},
+    // gpio b
+    {0xff},
+    // gpio c
+    {
+        //
+        1,   // blue led
+        0xff //
+    },
+    // gpio d
+    {0xff},
+#endif
+};
 
-int (*(commands[]))(char *content) = {NULL, write_gpio, NULL, read_gpio, reset};
+bool write_gpio(char *content);
+bool read_gpio(char *content);
+bool reset_gpio(char *content);
+
+bool (*(commands[]))(char *content) = {NULL, write_gpio, NULL, read_gpio, reset_gpio};
 
 int main(void)
 {
@@ -53,6 +101,8 @@ int main(void)
 
     printf("MCUX SDK version: %s\r\n", MCUXSDK_VERSION_FULL_STR);
     printf("start, check:%s\n", "set132487");
+
+    config();
 
     bool has_handled = false;
     while (1)
@@ -77,15 +127,10 @@ int main(void)
         }
     }
 }
-/*
-    返回值
-        <0  异常
-        0   未处理
-        1   正常
-*/
+
 #define argument_count (2)
 // write_gpio (2<<5)|1 1, blue led
-int write_gpio(char *content)
+bool write_gpio(char *content)
 {
     int r     = -1;
     int n     = -1;
@@ -95,7 +140,8 @@ int write_gpio(char *content)
     r = sscanf(buffer, " write_gpio %d %d %n", &pin, &value, &n);
     if ((r != argument_count) || (n != strnlen(buffer, buffer_size)))
     {
-        return 0;
+        printf("命令参数错误，或格式不完全匹配\n");
+        return false;
     }
     // XXX 是否存在
     // XXX 不同格式（241，J1-11，J1 11，J1_11，PTC1）
@@ -104,28 +150,17 @@ int write_gpio(char *content)
     int gpio_index = (pin >> 8) & 0xff;
     int pin_index  = (pin >> 0) & 0xff;
 
-    if (gpio_index < 0 || gpio_index >= 4 || pin_index < 0 || pin_index >= 32)
-    {
-        printf("索引超出可选范围：gpio_index=%d, pin_index=%d\n", gpio_index, pin_index);
-        return -1;
-    }
-
     // XXX -> 0/1
     value = (bool)value;
 
 #ifdef PORT_CLOCKS
     CLOCK_EnableClock(port_clocks[gpio_index]);
 #endif
-#ifdef GPIO_CLOCKS
-    CLOCK_EnableClock(gpio_clocks[gpio_index]);
-#endif
 #ifdef PORT_RSTS
     RESET_ReleasePeripheralReset(resets[gpio_index]);
 #endif
-
-    static const gpio_pin_config_t gpio_config = {.pinDirection = kGPIO_DigitalOutput, .outputLogic = 0U};
-    GPIO_PinInit(gpios[gpio_index], pin_index, &gpio_config);
-    PORT_SetPinConfig(ports[gpio_index], pin_index, &port_config_as_gpio);
+    GPIO_PinInit(gpios[gpio_index], pin_index, &gpio_pin_config_output_0);
+    PORT_SetPinConfig(ports[gpio_index], pin_index, &port_pin_config_gpio);
 
     GPIO_PinWrite(gpios[gpio_index], pin_index, value);
     printf(
@@ -139,15 +174,15 @@ int write_gpio(char *content)
     if (GPIO_PinRead(gpios[gpio_index], pin_index) != value)
     {
         printf("@%u, 设置失败\n", __LINE__);
-        return -2;
     }
-    return 1;
+
+    return true;
 }
 #undef argument_count
 
 #define argument_count (1)
 // read_gpio (2<<5)|1
-int read_gpio(char *content)
+bool read_gpio(char *content)
 {
     int r   = -1;
     int n   = -1;
@@ -155,18 +190,13 @@ int read_gpio(char *content)
     r       = sscanf(buffer, " read_gpio %d %n", &pin, &n);
     if ((r != argument_count) || (n != strnlen(buffer, buffer_size)))
     {
-        return 0;
+        printf("命令参数错误，或格式不完全匹配\n");
+        return false;
     }
     // 初始化为输入
 
     int gpio_index = (pin >> 8) & 0xff;
     int pin_index  = (pin >> 0) & 0xff;
-
-    if (gpio_index < 0 || gpio_index >= 4 || pin_index < 0 || pin_index >= 32)
-    {
-        printf("索引超出可选范围：gpio_index=%d, pin_index=%d\n", gpio_index, pin_index);
-        return -1;
-    }
 
 #ifdef PORT_CLOCKS
     CLOCK_EnableClock(port_clocks[gpio_index]);
@@ -174,9 +204,8 @@ int read_gpio(char *content)
 #ifdef PORT_RSTS
     RESET_ReleasePeripheralReset(resets[gpio_index]);
 #endif
-    static const gpio_pin_config_t config = {.pinDirection = kGPIO_DigitalInput};
-    GPIO_PinInit(gpios[gpio_index], pin_index, &config);
-    PORT_SetPinConfig(ports[gpio_index], pin_index, &port_config_as_gpio);
+    GPIO_PinInit(gpios[gpio_index], pin_index, &gpio_pin_config_output_0);
+    PORT_SetPinConfig(ports[gpio_index], pin_index, &port_pin_config_gpio);
 
     uint32_t value = GPIO_PinRead(gpios[gpio_index], pin_index);
     printf(
@@ -196,38 +225,19 @@ int read_gpio(char *content)
 #undef argument_count
 
 #define argument_count (0)
-int reset(char *content)
+bool reset_gpio(char *content)
 {
     int r = -1;
     int n = -1;
-    r     = sscanf(buffer, " reset %n", &n);
+    r     = sscanf(buffer, " reset_gpio %n", &n);
     if ((r != argument_count) || (n != strnlen(buffer, buffer_size)))
     {
-        return 0;
-    }
-    port_pin_config_t config = port_config_as_gpio;
-    config.mux               = kPORT_MuxAlt0;
-
-    int pins[4][32] = {
-        {0xff},
-        {0xff},
-        {0xff},
-        {0xff},
-    };
-    for (int i = 0, max_i = 4; i < max_i; ++i)
-    {
-        for (int j = 0, max_j = 32; j < max_j; ++j)
-        {
-            if (pins[i][j] == 0xff)
-            {
-                break;
-            }
-            PORT_SetPinConfig(ports[i], j, &config);
-        }
+        printf("命令参数错误，或格式不完全匹配\n");
+        return false;
     }
     // 初始化为输入
-    printf("reset\n");
-    return 1;
+    printf("reset gpio\n");
+    return true;
 }
 #undef argument_count
 
@@ -273,111 +283,3 @@ size_t __write(int handle, const unsigned char *buf, size_t bufSize)
         return SIZE_MAX - 3;
     }
 }
-
-#if 0
-static uint8_t pins[4][33] = {
-#if 1 // frdmmcxc353
-      // gpio 0
-    {
-        //
-        6,                           // i2c1 scl
-        7,                           // i2c1 sda
-        0xff,                        //
-        0, 1, 2, 3, 4, 5, 6, 7, 0xff //
-    },
-    // gpio 1
-    {
-        //
-        0,                                 // spi0 sck
-        1,                                 // spi0 sdi
-        2,                                 // spi0 sdo / pwt0 in0
-        3,                                 // spi0 pcs2 / pwm0 a0
-        0xff,                              //
-        0, 1, 2, 3, 8, 9, 10, 11, 12, 0xff //
-    },
-    // gpio 2
-    {
-#if 0 // debug console uart0
-            0, 1,
-#endif
-        0xff, 2, 3, 4, 5, 6, 7, 16, 17, 18, 19, 20, 21, 22, 23, 0xff //
-    },
-    // gpio 3
-    {0xff, 0, 1, 2, 3, 12, 13, 14, 15, 27, 28, 0xff},
-#endif
-#if 0 // kw47
-      // gpio a
-    {0xff},
-    // gpio b
-    {0xff},
-    // gpio c
-    {
-<<<<<<< HEAD
-        return SIZE_MAX - 3;
-    }
-}
-
-#include <stdarg.h>
-
-int scan(char *format, ...)
-{
-#define size (100)
-    static char buffer[size] = {0};
-    // static bool has_enter             = false;
-    char ch = '\0';
-    int i   = 0;
-    for (i = 0; i < size; ++i)
-    {
-        if (LPUART_ReadBlocking(LPUART1, (uint8_t *)&ch, 1) == kStatus_Success)
-        {
-            buffer[i] = ch;
-            if (ch == '\n')
-            {
-                break;
-            }
-        }
-        else
-        {
-            return -1;
-        }
-    }
-    buffer[i] = '\0';
-    va_list args;
-    va_start(args, format);
-    int r = vsscanf(buffer, format, args);
-    va_end(args);
-    return r;
-#undef buffer_size
-}
-int E(int n)
-{
-    int t = 1;
-    while (!(n & t))
-    {
-        t <<= 1;
-    }
-    return t;
-}
-int print(char *format, ...)
-{
-#define size (100)
-    static char buffer[size] = {0};
-    va_list args;
-    va_start(args, format);
-    int r = vsnprintf(buffer, size, format, args);
-    va_end(args);
-    r = printf("%s", buffer);
-    return r;
-#undef buffer_size
-}
-=======
-        //
-        1,   // blue led
-        0xff //
-    },
-    // gpio d
-    {0xff},
-#endif
-};
-#endif
->>>>>>> e17db0f (serial)
