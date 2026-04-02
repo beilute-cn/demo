5642dc6b6c83ac5c1a2357e7e766cc537694f692
beilute_cn <beilute_cn@163.com>
2026-04-02 21:43:25 +0800

    启动脚本

--- command/setup.bat
+++ command/setup.bat
@@ -1,3 +1,32 @@
 @echo off
 
-wt cmd /k doskey T=python "%cd%"\index.py $*
+setlocal enabledelayedexpansion
+
+chcp 65001
+
+echo cmdcmdline = %cmdcmdline%
+echo comspec = %comspec%
+
+if "!cmdcmdline!" == "!comspec!" (
+    echo 在命令行调用
+    doskey T=python "%cd%"\index.py $*
+) else (
+    echo 双击脚本文件
+    where wt >nul 2>nul
+    if %errorlevel% equ 0 (
+        echo win10没有windows terminal
+        cmd /k doskey T=python "%cd%"\index.py $*
+    ) else (
+        echo win11使用windows terminal
+        wt cmd /k doskey T=python "%cd%"\index.py $*
+    )
+)
+
+echo 按任意键退出
+doskey T=
+pause
+
+goto :eof
+
+
+
