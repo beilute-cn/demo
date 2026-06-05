d2d45ac64ee607349b97982833ffc16a5d973b52
Xiaohu Cao <xiaohu.cao@nxp.com>
2026-06-05 15:36:05 +0800

    编译需要一些库，不如直接使用msys的命令行

--- command/setup.cmd
+++ command/setup.cmd
@@ -13,7 +13,12 @@ doskey gdb2=call "%~dp0%gdb2.cmd"
 doskey haps=python "%~dp0%haps.py"
 doskey rdapeng=python "%~dp0%dapeng.py"
 doskey command=call "%~dp0%command.cmd"
+@REM doskey gcc="C:\sys\exe\msys64\ucrt64\bin\gcc.exe" $*
+@REM doskey g++="C:\sys\exe\msys64\ucrt64\bin\g++.exe" $*
 @REM doskey make="C:\sys\exe\msys64\ucrt64\bin\mingw32-make.exe" $*
+@REM  -help看所有参数
+@REM 可以在字符串中主动添加其他内容来固定格式，比如：x-help
+doskey ucrt="C:\sys\exe\msys64\msys2_shell.cmd" -ucrt64 -here
 prompt [%command%] $P$G
 
 call "%~dp0%config.cmd" disable
